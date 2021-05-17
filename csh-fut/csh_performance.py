3db1184604910f9cdfdc9940f327521754d01564
Kristian Andreasen <kristianbojer@hotmail.com>
2021-05-17 16:04:37 +0200

    misc changes

--- csh-fut/csh_performance.py
+++ csh-fut/csh_performance.py
@@ -7,11 +7,11 @@ import csh_main
 
 debug=1
 #limit = 5
-k_list = [1,8]
-stepsize_iterations = 2
-max_iterations = 6
-max_pairs = 1
-patch_size = 8
+k_list = [1,4,8]
+stepsize_iterations = 1
+max_iterations = 7
+max_pairs = 133
+ps = 16
 
 directory_path = sys.argv[1]
 entries = sorted(os.listdir(directory_path))
@@ -35,12 +35,12 @@ def run_pair(path1, path2):
         for i in range(1,max_iterations, stepsize_iterations):
             if debug: print("Iterations {}, Knn {}".format(i,j))
             time1 = time.perf_counter()
-            res = csh_main.csh_knn(img_a_conv, img_b_conv, i,j, patch_size=patch_size)
+            res = csh_main.csh_knn(img_a_conv, img_b_conv, i,j, patch_size=ps)
             time2 = time.perf_counter()
-            best_matches = csh_main.find_best_n(res, img_a_u8, img_b_u8, patch_size=patch_size )
+            best_matches = csh_main.find_best_n(res, img_a_u8, img_b_u8,patch_size=ps)
             time3 = time.perf_counter()
-            error = csh_main.mean_error(best_matches, img_a_u8, img_b_u8, patch_size=patch_size)
-            result_file.write("{},{},{}\n".format(time2-time1, time3-time2, error))
+            error = csh_main.mean_error(best_matches, img_a_u8, img_b_u8, patch_size=ps)
+            result_file.write("{},{},{},{},{}\n".format(i, j, time2-time1, time3-time2, error))
 
 pair_count = 0
 for i,k in zip(entries[0::2],entries[1::2]):
